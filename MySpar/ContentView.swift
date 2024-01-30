9a2bd472f698d1e7b1478f2ceec8572c6d1ea903
DmitryGorbunow <dmitry.gorbunow@gmail.com>
2024-01-30 15:53:55 +0500

    Initial Commit

---
+++ MySpar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySpar
+//
+//  Created by Dmitry Gorbunow on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Dmitry Gorbunow on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
